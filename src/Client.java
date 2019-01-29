78af4f6c9fce2268d8be3b4c38e898d99c5f6b23
Matan Halaly <46110457+maithen@users.noreply.github.com>
2019-01-30 00:43:43 +0200

    Update Client.java
    added comments to all things done in client.

--- src/Client.java
+++ src/Client.java
@@ -8,34 +8,54 @@ public class Client {
 	private float  interestRate=0;;
 	private Logger logger;
 	Account[] account = new Account[5];
-
-	// Adding an account to a Client
+	
+	// Constructor
+	public Client(int clientId, String clientName, float clientBalance) {
+		setCid(clientId);
+		setName(clientName);
+		setBalance(clientBalance);
+		logger = new Logger("matan");
+	
+	}
+	// Adding an account to a Client(does not allow duplicate ID's)
 	public void addAccount(int id, float balance) {
+		for(int j=0;j<account.length;j++) {
+			if (account[j]!=null && id==account[j].getId()){
+				System.out.printf("Account ID: %d already exists. pick a new ID to create an account. \n", account[j].getId());
+				return;
+			}
+		}	
 		for(int i=0;i<account.length;i++) {
 			if(account[i]==null) {
-			account[i]= new Account(id,balance);
-			Logger logger;
-			break;
+				account[i]= new Account(id,balance);
+				Logger logger;
+				return;
+					}
+				}
+			}		
+	// Getting all Accounts.
+	public String getAccounts(){
+		String gA = "";
+		for(int i=0;i<account.length;i++) {
+			if(account[i]!=null) {
+				gA +=  String.format("%s \n", account[i]);
 			}
-		
-	}
+		}
+		return gA;
 	}
 	// Getting Account details by an account ID
-	public void getAccount(int id) {
-		for(int i=0;i<account.length;i++) {
-			if(id==account[i].getId()){
-			System.out.printf("account id : %s has a balance of %s \n", account[i].getId(),account[i].getBalance());
-			return;
+	public String getAccount(int accountId) {
+		for(int i=0;i<account.length;i++) {		
+			if(accountId==account[i].getId()){
+				return String.format("%s \n" ,account[i]);
 			}
-			}
-			System.out.println("does not exist!");
-			return;
-		}
+		}	
+		return "hehe";
+	}
 	// Removing an account by an account ID
 	public void removeAccount(int id) {
 		for(int i=0;i<account.length;i++) {
 			if(id==account[i].getId()){
-				System.out.println("Account Deleted.");
 				balance += account[i].getBalance();
 				logger = new Logger("");
 				Log log = new Log(System.currentTimeMillis(), getCid(), "customer has deleted account id:" + account[i].getId());
@@ -54,13 +74,14 @@ public class Client {
 	public void withdraw(float wd) {
 		balance -= wd+comissionRate;
 	}
-	// Auto updating the amount of interest to add on each Client's accounts
+	// Auto-updating the amount of interest to add on each Client's accounts
 	public void autoUpdateAccounts() {
 		for(int i=0;i<account.length;i++) {
-			account[i].getBalance()  
+			if(account[i]!=null)
+			 account[i].setBalance((account[i].getBalance()+ getInterestRate()));
+			Logger logger;
 		}
-		
-	}
+		}
 	// Returning the sum of a client balance + total accounts balance.
 	public float getFortune() {
 		float sum=0;
@@ -73,14 +94,8 @@ public class Client {
 	}
 	
 
-	public Client(int id, String name, float balance) {
-		setCid(id);
-		setName(name);
-		setBalance(balance);
-		Logger logger;
-	
-	}
-	
+
+	//Getters/Setters
 	public int getCid() {
 		return cid;
 	}
