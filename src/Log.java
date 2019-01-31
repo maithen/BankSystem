9296733be9dcd382f7ac93f372f766ba5ba5ea3a
Matan Halaly <46110457+maithen@users.noreply.github.com>
2019-01-31 22:23:50 +0200

    actually looks good now and prints well

--- src/Log.java
+++ src/Log.java
@@ -5,27 +5,28 @@ public class Log {
 	private long timestamp;
 	private int clientId;
 	private String description;
+	private float amount;
 	
 	
 	
-	
-	public Log(long timestamp, int clientId , String description) {
+
+	public Log(long timestamp, int clientId, String description, float amount) {
+		
 		this.timestamp = timestamp;
 		this.clientId = clientId;
 		this.description = description;
-		
-
+		this.amount = amount;
 	}
 	
+	// Prints the log
 	public String getData() {
-		return  String.format(" Time : %s, Client ID : %d, Description : %s .",timestamp, clientId, description);
+		return  String.format("Time: %s, Client ID: %d, Description: '%s', Amount: %f. ",timestamp, clientId, description, amount);
 	}
 	
-
-
 	@Override
 	public String toString() {
-		return "Log [timestamp=" + timestamp + ", clientId=" + clientId + ", description=" + description + "]";
+		return String.format("Log [timestamp=%s, clientId=%s, description=%s, amount=%s]", timestamp, clientId,
+				description, amount);
 	}
 	
 	
