20dfbb7c882f2fe374647dba2a8f94ba5408bd79
Matan Halaly <46110457+maithen@users.noreply.github.com>
2019-02-01 17:37:06 +0200

    some checks

--- src/Bank.java
+++ src/Bank.java
@@ -1,40 +1,60 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Bank {
 	
-	Client[] clients = new Client[100];
+	private Client[] clients;
 	private Logger logService;
-	//->Account updater that I'm leaving for later
+	private Logger logger = new Logger("DriverName");
 	private float balance;
+
 	
 	
 	//Constructor (empty for now)
 	public Bank() {
 	
-			
+			clients = new Client[100];
 			
 		}
+	
+	// Starts the account updater (empty for now)
+	public void startAccountUpdater() {}
 	
 	//Adding a client to the array
 	public void addClient(int id, String name, float balance) {
-		for(int i=0;i<clients.length;i++) {
-			if(clients[i]==null) {
-				clients[i] = new Client(id, name, balance);
-				return;
-			}
-	}
-	}
-	
-	//removing a client from the array
-	public void removeClient(int id) {
-		for(int i=0;i<clients.length;i++) {
-			if(id==clients[i].getCid()) {
-				clients[i]=null;
-				Logger logger;
+		for(int j=0;j<clients.length;j++) {
+			if (clients[j]!=null && id==clients[j].getCid()) {
+				System.out.printf("Client ID: %d already exists. \n", id);
 				return;
 			}
 		}
+		for(int i=0;i<clients.length;i++) {
+			if(clients[i]==null) {
+			clients[i] = new Client(id, name, balance);	
+			Log log = new Log(System.currentTimeMillis(), clients[i].getCid(), "Client has been added to the bank.", balance);
+			logger.log(log);
+			return;
+		}
+		}
 	}
+
+	//removing a client from the array
+	public void removeClient(int id) {
+		for(int i=0;i<clients.length;i++) {
+			if(clients[i]!= null && id == clients[i].getCid()) {
+				Log log = new Log(System.currentTimeMillis(), clients[i].getCid(), "Client  has been removed.", clients[i].getBalance());
+				logger.log(log);
+				clients[i]=null;
+				return;
+			}	
+			}
+		System.out.printf("Client ID: %d does not exist. \n", id);
+		}
 	
+	// Retrieving the bank's balance
 	public float getBalance() {
 		int sum=0;
 		for(int i=0;i<clients.length;i++) {
@@ -45,10 +65,63 @@ public class Bank {
 		return sum+balance;
 	}
 	
-	public Client[] getClients() {
-		return Logger(log);
-		
+	// Supposed to retrieve clients (shows memory allocation for now), 
+	// done differently with void instead of Client[] like the book asks.
+	public void getClients() {
+			for(int i=0;i<clients.length;i++) {
+				if(clients[i]!=null) {
+		System.out.println(clients[i]);
+		}
+	}
+	}
+	
+	
+	@Override
+	public String toString() {
+		return String.format("Bank [clients=%s, logService=%s, balance=%s]", Arrays.toString(clients), logService,
+				balance);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Float.floatToIntBits(balance);
+		result = prime * result + Arrays.hashCode(clients);
+		result = prime * result + ((logService == null) ? 0 : logService.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Bank other = (Bank) obj;
+		if (Float.floatToIntBits(balance) != Float.floatToIntBits(other.balance))
+			return false;
+		if (!Arrays.equals(clients, other.clients))
+			return false;
+		if (logService == null) {
+			if (other.logService != null)
+				return false;
+		} else if (!logService.equals(other.logService))
+			return false;
+		return true;
 	}
 	
 
+
+
+
 }
+
+
+		
+
+	
+
+
